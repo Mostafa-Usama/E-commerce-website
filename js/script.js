3597d3135d99f055b43ba3a2920068b9435aa9d9
Mostafa-Usama <mostafa.usama286@gmail.com>
2024-01-10 06:39:46 +0200

    added cart functionality, clear cart bug yet to be fixed

--- js/script.js
+++ js/script.js
@@ -6,9 +6,19 @@ const user = document.getElementById("user")
 const cart = document.getElementById('cart')
 let isSigned = localStorage.getItem("signed") === "true"
 let btns
+let itemCon = document.getElementById("items-con")
+let opened = false
+let fav = false
 
 logoutLink.addEventListener('click', ()=> localStorage.setItem("signed", "false"))
+cart.addEventListener("click", showItems)
 
+function showItems(e) {
+    if (e.target !== e.currentTarget) return;
+    opened ? itemCon.style.display = "none" : itemCon.style.display = "flex"
+    opened = !opened
+
+}
 window.onload = function () {
     if (isSigned) {
         login.style.display = "none"
@@ -20,15 +30,152 @@ window.onload = function () {
 
     }
     btns = document.getElementsByClassName("cartBtn")
-    Array.from(btns).forEach(function (btn) {
-        btn.addEventListener("click", addToCart)    
-    })
+    draw()
 }
+let itemsInCart = []
 
-function addToCart(event) {
+function addToCart(event, id) {
     if (!isSigned) {
         location.assign("login.html")
     }
-    let i = Array.from(btns).indexOf(event["target"])
+    else {
+        let amount = document.getElementById(`amount${id}`)
+        amount = +amount.value
+        if (amount > 0 && amount < 100) {
+            let i = id
+            products[i].amount += amount
+            let index
+            if (itemsInCart.some((value, ind) => {
+                index = ind
+                return value.name === products[i].name
+            })) {
+                itemsInCart[index].amount = products[i].amount
+            }
+            else {
+                itemsInCart.push({
+                    name: products[i].name,
+                    amount: products[i].amount,
+                    id: products[i].id
+                })
+            }
+            let toastElList = document.querySelector('.toast')
+            let body = document.querySelector('.toast-body')
+            let toast = new bootstrap.Toast(toastElList)
+            body.textContent = `You added ${products[i].name} to your Cart!`
+            toast.show()
 
+            let btn = event["target"] 
+            btn.onclick = null
+            btn.onclick = ()=> {
+                removeFromCart(event, id)
+            }
+            btn.textContent = "Remove From Cart"
+       
+            draw()
+        }
+    }
+
+}
+
+function removeFromCart(event, id) {
+    let btn = event["target"]
+    let i = id
+    console.log(i)
+    products[i].amount = 0
+    let index = itemsInCart.findIndex((ele) => ele.id === id)
+    itemsInCart.splice(index, 1)
+    btn.onclick = null
+    btn.onclick = function x() {
+        addToCart(event, i)
+    }
+    btn.textContent = "Add To Cart"
+
+    let toastElList = document.querySelectorAll('.toast')
+    let body = document.querySelectorAll('.toast-body')
+    let toast = new bootstrap.Toast(toastElList[1])
+    body[1].textContent = `You Removed ${products[i].name} from your Cart!`
+    toast.show()
+
+    draw()
+}
+
+function draw() {
+    itemCon.innerHTML = ""
+    if (itemsInCart.length == 0) {
+        let p = document.createElement("p")
+        p.textContent = "Cart Empty"
+        p.style.textAlign = "center"
+        p.style.fontSize = "16px"
+        itemCon.appendChild(p)
+    }
+    else {
+        for (let i = 0; i < itemsInCart.length; i++) {
+            itemCon.innerHTML += `
+            <div class="d-flex mb-2 bg-secondary bg-opacity-10 justify-content-between align-items-center  px-2 py-3 rounded" style="min-width:250px;">
+                <p class="m-0 fs-6" >${itemsInCart[i].name}</p>
+                <div class=''>
+                    <span  onclick="remove(event, ${itemsInCart[i].id})" class="px-2" style="cursor: pointer; color: red;">-</span>
+                    <span class="m-0 fs-6">${itemsInCart[i].amount}</span>
+                    <span onclick="add(event, ${itemsInCart[i].id})" style="cursor: pointer; color: green;" class="px-2 fw-bold">+</span>
+                </div>
+                </div>
+         
+            `
+        }
+        itemCon.innerHTML += ` <button onclick="clearCart(event)"  class=" btn rounded-0 fw-bold btn3 bg-white" style="transition: .3s; color: var(--primary); border: 2px solid var(--primary); ">Clear Cart</button>`
+    }
+}
+
+function add(event, id) {
+    products[id].amount += 1
+    let index = itemsInCart.findIndex((ele) => ele.id === id)
+    itemsInCart[index].amount = products[id].amount
+    event["target"].previousElementSibling.textContent = products[id].amount 
+}
+function remove(event, id) {
+    products[id].amount -= 1
+    let index = itemsInCart.findIndex((ele) => ele.id === id)
+    itemsInCart[index].amount = products[id].amount
+    if (products[id].amount == 0) {
+        itemsInCart.splice(index, 1)
+        let btn = event["target"]
+        console.log(btn, event)
+        btn.removeEventListener("click", removeFromCart)
+        btn.onclick = function () {
+            addToCart(event, id)
+        }
+        btn.textContent = "Add To Cart"
+
+        draw()
+        
+    }
+    else {
+        event["target"].nextElementSibling.textContent = products[id].amount
+    }
+}
+
+function clearCart(event) {
+    products.forEach((ele) => ele.amount = 0)
+    itemsInCart.splice(0)
+    Array.from(btns).forEach((btn, i) => {
+        btn.removeEventListener("click", removeFromCart)
+        btn.onclick = function () {
+            addToCart(event, i)
+        }
+        btn.textContent = "Add To Cart"
+    })
+
+    draw()
+}
+function reset(event) {
+    if (!event["target"].value) {
+        event["target"].value = 1
+    }
+    console.log(event["target"].value)
+}
+
+
+function favourite(event, id) {
+    products[id].fav ? event["target"].style.color = "" : event["target"].style.color = "red"
+    products[id].fav = !products[id].fav
 }
--- js/search.js
+++ js/search.js
@@ -2,13 +2,15 @@ const Pname = document.getElementById("Pname")
 const catagory = document.getElementById("Pcatagory")
 const budget = document.getElementById("budget")
 const productsContainer = document.getElementById("productsContainer")
-let prodcuts = [
+let products = [
     {
         id: 0,
         name: "Golden Watch",
         image: "images/watch1.png",
         price: "$200.00", 
-        catagory: "Accessories"
+        catagory: "Accessories",
+        amount: 0,
+        fav: false
 
     },
     {
@@ -16,7 +18,9 @@ let prodcuts = [
         name: "Silver Watch",
         image: "images/watch2.png",
         price: "$100.00", 
-        catagory: "Accessories"
+        catagory: "Accessories",
+        amount: 0,
+        fav: false
 
     },
     {
@@ -24,7 +28,9 @@ let prodcuts = [
         name: "Brown Shoes",
         image: "images/shoes.png",
         price: "$80.00", 
-        catagory: "Fashion"
+        catagory: "Fashion",
+        amount: 0,
+        fav: false
 
     },
     {
@@ -32,7 +38,9 @@ let prodcuts = [
         name: "Grey Hoodie",
         image: "images/hoodie1.png",
         price: "$100.00", 
-        catagory: "Fashion"
+        catagory: "Fashion",
+        amount: 0,
+        fav: false
 
     },
     {
@@ -40,7 +48,9 @@ let prodcuts = [
         name: "Brown Sunglasses",
         image: "images/sunglasses2.png",
         price: "$70.00", 
-        catagory: "Accessories"
+        catagory: "Accessories",
+        amount: 0,
+        fav: false
 
     },
     {
@@ -48,7 +58,9 @@ let prodcuts = [
         name: "White Cap",
         image: "images/cap1.png",
         price: "$30.00", 
-        catagory: "Sport"
+        catagory: "Sport",
+        amount: 0,
+        fav: false
 
     },
     {
@@ -56,7 +68,9 @@ let prodcuts = [
         name: "Football Shoes",
         image: "images/football1.png",
         price: "$100.00", 
-        catagory: "Sport"
+        catagory: "Sport",
+        amount: 0,
+        fav: false
 
     },
     {
@@ -64,7 +78,9 @@ let prodcuts = [
         name: "Blue T-Shirt",
         image: "images/shirt1.png",
         price: "$60.00", 
-        catagory: "Fashion"
+        catagory: "Fashion",
+        amount: 0,
+        fav: false
 
     },
     {
@@ -72,7 +88,9 @@ let prodcuts = [
         name: "Grey Pants",
         image: "images/pants2.png",
         price: "$50.00",
-        catagory: "Fashion"
+        catagory: "Fashion",
+        amount: 0,
+        fav: false
 
     },
     {
@@ -80,7 +98,9 @@ let prodcuts = [
         name: "Headphones",
         image: "images/headphones1.png",
         price: "$120.00",
-        catagory: "Accessories"
+        catagory: "Accessories",
+        amount: 0,
+        fav: false
 
     },
     {
@@ -88,7 +108,9 @@ let prodcuts = [
         name: "Argentina Shirt",
         image: "images/Argentina.png",
         price: "$110.00",
-        catagory: "Sport"
+        catagory: "Sport",
+        amount: 0,
+        fav: false
 
     },
     {
@@ -96,20 +118,20 @@ let prodcuts = [
         name: "Addidas Shorts",
         image: "images/shorts2.png",
         price: "$75.00",
-        catagory: "Sport"
+        catagory: "Sport",
+        amount: 0,
+        fav: false
 
-    },
+    }
 
 ]
-let productsToDraw = [...prodcuts]
-
+let productsToDraw = [...products]
 Pname.addEventListener("keyup", filterInput)
 catagory.addEventListener("keyup", filterInput)
 budget.addEventListener("keyup", filterInput)
 
 function filterInput() {
-    console.log(Pname.value)
-    productsToDraw = prodcuts.filter((product) => product.name.toLowerCase().includes(Pname.value.toLowerCase().trim()))
+    productsToDraw = products.filter((product) => product.name.toLowerCase().includes(Pname.value.toLowerCase().trim()))
     productsToDraw = productsToDraw.filter((product) => product.catagory.toLowerCase().includes(catagory.value.toLowerCase().trim()))
     if (budget.value) {
         productsToDraw = productsToDraw.filter((product) => {
@@ -124,34 +146,66 @@ function filterInput() {
 function renderProducts() {
     
     productsContainer.textContent = ""
-    for (let i = 0; i < productsToDraw.length; i++){
+    for (let i = 0; i < products.length; i++){
+        if (productsToDraw.indexOf(products[i]) != -1)
         productsContainer.innerHTML += `
         <div class="col-md-6 col-lg-3 box"  >
         <div class="text-center rounded" >
             <div class="image overflow-hidden position-relative">
-                <img src="${productsToDraw[i].image}" loading="lazy" class="img-fluid  bg-secondary bg-opacity-10 rounded-top" alt="Golden Watch">
+                <img src="${products[i].image}" loading="lazy" class="img-fluid  bg-secondary bg-opacity-10 rounded-top" alt="Golden Watch">
             </div>
             <div class="px-4 py-4 border border-top-0">
-                <h5 class="fw-bold">${productsToDraw[i].name}</h5>
-                <p class="opacity-75 mb-1 " style="font-weight:600;">Price: ${productsToDraw[i].price}</p>
+                <h5 class="fw-bold">${products[i].name}</h5>
+                <p class="opacity-75 mb-1 " style="font-weight:600;">Price: ${products[i].price}</p>
                 <i class="fas fa-star py-2" style="color: rgb(255, 192, 34);"></i>
                 <i class="fas fa-star" style="color: rgb(255, 192, 34);"></i>
                 <i class="fas fa-star" style="color: rgb(255, 192, 34);"></i>
                 <i class="fas fa-star" style="color: rgb(255, 192, 34);"></i>
                 <i class="fas fa-star" style="color: rgb(255, 192, 34);"></i>
-                <p class="opacity-75">Catagory: ${productsToDraw[i].catagory}</p>
+                <p class="opacity-75">Catagory: ${products[i].catagory}</p>
+                <span class='opacity-75 fw-bold'>Quantity: </span>
+                <input type="number" id='amount${products[i].id}'  onblur="reset(event)" min="1" max="99" value="1"  class='fw-bold mb-3 text-center' style='width:40px; '>
                 <div class="d-flex justify-content-between align-items-center">
-                    <button  class="cartBtn btn rounded-0 fw-bold btn3 bg-white" style="transition: .3s; color: var(--primary); border: 2px solid var(--primary); ">Add to cart</button>
-                    <i class="fas fa-heart fa-2x text-secondary" style="cursor: pointer;"></i>
+                    <button onclick="addToCart(event, ${products[i].id})"  class="cartBtn btn rounded-0 fw-bold btn3 bg-white" style="transition: .3s; color: var(--primary); border: 2px solid var(--primary); ">Add to cart</button>
+                    <i onclick='favourite(event, ${products[i].id})' class=" fas fa-heart fa-2x " style="transition:.3s; cursor: pointer;"></i>
                 </div>
             </div>
         </div>
     </div>
         `
     }
+    productsContainer.innerHTML += `
+                <div class="toast-container position-fixed bottom-0 end-0 p-3 text-white ">
+                    <div id="liveToast" class="toast bg-success " role="alert" aria-live="assertive" aria-atomic="true">
+                        <div class="toast-header bg-success text-white">
+                            <i class="fas fa-check-circle pe-2"></i>
+                            <strong class="me-auto">Item Added!</strong>
+                            <small>1 min ago</small>
+                            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                        </div>
+                        <div class="toast-body">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="toast-container position-fixed bottom-0 end-0 p-3 text-white ">
+                    <div id="liveToast" class="toast bg-danger " role="alert" aria-live="assertive" aria-atomic="true">
+                        <div class="toast-header bg-danger text-white">
+                            <i class="fas fa-check-circle pe-2"></i>
+                            <strong class="me-auto">Item Removed!</strong>
+                            <small>1 min ago</small>
+                            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                        </div>
+                        <div class="toast-body">
+
+                        </div>
+                    </div>
+                </div>`
+    
+    
     if (productsToDraw.length == 0) {
         let h2 = document.createElement("h2")
-        h2.textContent = "No prodcuts were found!"
+        h2.textContent = "No products were found!"
         h2.style.textAlign = "center"
         productsContainer.appendChild(h2)
     }
